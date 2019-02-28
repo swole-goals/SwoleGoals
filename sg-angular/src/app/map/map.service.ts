849e3549e184cf26271b790e48f5ea9e5ccaea7d
Michael <mdlawrence97@gmail.com>
2019-02-28 17:09:45 -0500

    Image url issue when image not found, added exercise names to map

--- sg-angular/src/app/map/map.service.ts
+++ sg-angular/src/app/map/map.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
+import { ExerciseInfo } from '../exercise-list/exerciseinfo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getExerciseListUnfiltered() {
+    return this.httpClient.get(`https://swolegoalsdatabase.appspot.com/getEx`).
+      pipe(
+        map((item: any) => item.map(e => <ExerciseInfo>
+        {
+          name: e.name,
+          type: e.type,
+          muscles: e.muscles,
+          equipment: e.equipment,
+          level: e.level,
+          review: e.review,
+          instructions: e.instructions,
+          image1: e.image2,
+          image2: e.image2
+        })));
+  }
 }
+
