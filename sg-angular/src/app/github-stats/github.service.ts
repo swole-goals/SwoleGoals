c777fc7ecf5233a90280a53c82be054cf80cdf02
Rohan Koripalli <rkoripalli@hotmail.com>
2019-02-28 14:27:07 -0600

    Refactored github-stats and removed Angular picture

---
+++ sg-angular/src/app/github-stats/github.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { GitRepoInfo } from './gitrepoinfo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {   
+
+    constructor(private httpClient: HttpClient) { }
+
+    getUserRepos(): Observable<GitRepoInfo> {
+        return this.httpClient.get(`https://api.github.com/repos/vivianmnguyen/SwoleGoals/stats/contributors`).
+            pipe(
+                map((item: any) => item.map(p => <GitRepoInfo>
+                    {
+                        user: p.author.login,
+                        url: p.author.html_url,
+                        commits: p.total
+                    })));            
+    }
+}
---
+++ sg-angular/src/app/github-stats/gitrepoinfo.ts
@@ -0,0 +1,8 @@
+export class GitRepoInfo {
+    user: string;
+    name: string;
+    url: string;
+    commits: string;
+    bio: string;
+    image: string;
+}
