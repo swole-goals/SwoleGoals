10a61ca977817d983caaa86b94c840f76ba579ae
Michael <mdlawrence97@gmail.com>
2019-03-28 17:59:38 -0400

    12 test cases on sql database

---
+++ testing/testStrengthLevel.js
@@ -0,0 +1,85 @@
+// run this
+//./cloud_sql_proxy -instances=swolegoalsdatabase:us-central1:swolegoalsdb=tcp:3307
+// make sure you have node mysql installed with "npm install mysql"
+// run this as "node server.js"
+var fs = require('fs');
+var assert = require('assert');
+ 
+var mysql = require('mysql');	
+var con = mysql.createConnection({
+     host     : 'localhost',
+     port     : 3307,
+     user     : 'root',
+     password : 'swolegoals',
+     database : 'exercises'
+});
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+//var cmd = " create table clean_exercises( id INT NOT NULL AUTO_INCREMENT, name VARCHAR(100), type VARCHAR(100), muscles VARCHAR(100), equipment VARCHAR(100), level VARCHAR(100), review VARCHAR(100), instructions VARCHAR(5000), image1 VARCHAR(100), image2 VARCHAR(100), PRIMARY KEY ( id ) ) ";
+var testSelectNoFilters = function() {
+	var cmd = "select * from strengthlevel";
+	con.query(cmd, function (err, result) {
+		assert(result.length > 0);
+	});
+	console.log("successfully tested selectNoFilters");
+}
+var testSelectFilteredByName = function() {
+	var cmd = "select exercise_name from strengthlevel";
+	con.query(cmd, function (err, result) {
+		assert(result.length > 0);
+	});
+	console.log("successfully tested selectFilteredByName");
+}
+var testSelectBadFilter = function() {
+	var cmd = "select fake from strengthlevel";
+	con.query(cmd, function (err, result) {
+		assert(result == null);
+	});
+	console.log("successfully tested selectBadFilter");
+	
+}
+var testNamesAreDistinct = function() {
+	var cmd = "select exercise_name from strengthlevel";
+	var names = [];
+	con.query(cmd, function (err, result) {
+		result.forEach(function(name) {
+			names.forEach(function(n) {
+				assert(name !== n);
+			});
+			names.push(name);
+		});
+	});
+	console.log("successfully tested selectNamesAreDistinct");
+}
+var testExercisesContainPerformance = function() {
+	var cmd = "select * from strengthlevel";
+	con.query(cmd, function (err, result) {
+		result.forEach(function(exercise) {
+			assert(exercise.performance_a !== null);
+		});
+	});
+	console.log("successfully tested exercisesContainMuscles");
+}
+var testPrimaryKeysAreDistinct = function() {
+	var cmd = "select exercise_id from strengthlevel";
+	var ids = [];
+	con.query(cmd, function (err, result) {
+		result.forEach(function(id) {
+			ids.forEach(function(n) {
+				assert(id !== n);
+			});
+			ids.push(id);
+		});
+	});
+	console.log("successfully tested primaryKeysAreDistinct");
+}
+
+testSelectNoFilters(); 
+testSelectFilteredByName();
+testSelectBadFilter();
+testNamesAreDistinct(); 
+testExercisesContainPerformance();
+testPrimaryKeysAreDistinct();
+
