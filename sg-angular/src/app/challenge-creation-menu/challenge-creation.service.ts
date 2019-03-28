1c2a67d699e209740a61a928bcc303a73fc9c42a
Nick Pattie <32149718+NickPattie@users.noreply.github.com>
2019-03-28 18:04:52 -0500

    challenge creation service

---
+++ sg-angular/src/app/challenge-creation-menu/challenge-creation.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChallengeCreationService {
+
+  constructor(private httpClient : HttpClient) { }
+  postAPIdata(challengeData){
+    console.log(challengeData);
+    return this.httpClient.post('http://localhost:8080/addChallenge',
+      {'exercises' : `${challengeData[0]}`, 'reps' : `${challengeData[1]}`});
+  }
+
+}
