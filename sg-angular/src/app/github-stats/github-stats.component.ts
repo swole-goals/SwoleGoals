cc42a40f27360e85663d1778d2e58378d8a586a4
Michael <mdlawrence97@gmail.com>
2019-03-30 19:40:44 -0400

    all commits are now showing, this was way harder than expected

--- sg-angular/src/app/github-stats/github-stats.component.ts
+++ sg-angular/src/app/github-stats/github-stats.component.ts
@@ -7,26 +7,7 @@ import { GithubService } from '../github-stats/github.service';
     styleUrls: ['./github-stats.component.css']
 })
 export class GithubStatsComponent implements OnInit {
-
-    public gitAccountList;
-    public totalCommits = 0;
-    public issues;
-    constructor(private githubserv: GithubService) { }
-
-    ngOnInit() {
-    	this.loadGithubRepos();
-	this.loadGithubIssues();
-    }
-	public loadGithubIssues() {
-		this.githubserv.getIssues().subscribe((data) => {
-			this.issues = data;	
-		});
-	}
-    public loadGithubRepos() {
-        this.githubserv.getUserRepos().subscribe((data) => {
-            this.gitAccountList = data;
-
-            this.gitAccountList.forEach(i => {
+/*
                 if(i.user === "vivianmnguyen"){
                     i.name = "Vivian Nguyen";
                     i.bio = "Vivian is an ECE Major graduating in December of 2019";
@@ -72,9 +53,151 @@ export class GithubStatsComponent implements OnInit {
                     i.image = "../../assets/headshots/rohan.png"
                     i.responsibilities = "Create angular project. Setup Bootstrap. Build Splash page and About page " +
                       "using Github API. Setup routing between Angular components."
+*/
+public gitAccountList = [	
+					{
+						user: 'vivianmnguyen',
+						url: '',
+						commits: '0',
+						name: 'Vivian Nguyen',
+						bio: 'Vivian is an ECE Major graduating in December of 2019',
+						image: '../../assets/headshots/vivian.jpg',
+						responsibilities: "Initialize CloudSQL database. Load web-scraped exercise data into database. " +
+                      "Write notes for local database development. " +
+                      "Write a script for automatically loading web-scraped text files"
+					},
+					{
+						user: 'KaiboCai',
+						url: '',
+						commits: '0',
+						name: 'Kaibo Cai',
+						bio: 'Kaibo is an ECE Major graduating in December of 2019',
+						image: '../../assets/headshots/kaibo.jpg',
+						responsibilities: "Set up the backend server using nodejs.  " +
+                      "Connect the backend server with glcoud sql database. " +
+                      "Create two Get request routers. Create user table in the database."
+					},
+					{
+						user: 'NFinks',
+						url: '',
+						commits: '0',
+						name: 'Nicole Finks',
+						bio: 'Nicole is an ECE Major graduating in December of 2019',
+						image: '../../assets/headshots/nicole.jpg',
+						responsibilities: "Create profile page using Bootstrap 4. Create basic aesthetic design to be " +
+                      "used throughout front end. Standardization of css files. " +
+                      "Correction of general front-end functionality errors."
+					},
+					{
+						user: 'prilak',
+						url: '',
+						commits: '0',
+						name: 'Michael Lawrence',
+						bio: 'Michael is an ECE Major graduating in May of 2019',
+						image: '../../assets/headshots/michael.jpg',
+						responsibilities: "Create map page and exercise list page. " +
+                      "Add issue information on the about page. Work on initializing the scraping script."
+					},
+					{
+						user: 'NickPattie',
+						url: '',
+						commits: '0',
+						name: 'Nick Pattie',
+						bio: 'Nick is a BME Major graduating in May of 2019',
+						image: '../../assets/headshots/nick.png',
+						responsibilities: "Write Python script for webscraping on Bodybuilding.com utilizing selenium. " +
+                      "Create test file for web scraping. " +
+                      "Provide and load in exersise data into MySQL database."
+					},
+					{
+						user: 'rkoripalli',
+						url: '',
+						commits: '0',
+						name: 'Rohan Koripalli',
+						bio: 'Rohan is an ECE Major graduating in December of 2019',
+						image: '../../assets/headshots/rohan.png',
+						responsibilities: "Create angular project. Setup Bootstrap. Build Splash page and About page " +
+                      "using Github API. Setup routing between Angular components."
+					}
+					];
+    public totalCommits = 0;
+    public issues;
+    public commits = [];
+    constructor(private githubserv: GithubService) { }
+   	
+    ngOnInit() {
+    	this.loadGithubRepos();
+	this.loadGithubIssues();
+    }
+	public loadGithubIssues() {
+		this.githubserv.getIssues().subscribe((data) => {
+			this.issues = data;	
+		});
+	}
+	public loadService(page){
+		var urlString = `https://api.github.com/repositories/168768624/commits?page=`;
+		this.githubserv.getUserCommits(urlString, page).subscribe(item => {
+		var cs = item as Array<any>;
+		//console.log(item.length);
+		//	for(let item of item)
+			
+			if(cs.length != null && cs.length > 0) {
+				cs.forEach(commit => {this.commits.push(commit)});	
+				this.loadService(++page);
+				console.log(this.commits);
+				}
+				
+			else {//done
+				var commitCounts = [0, 0, 0, 0, 0, 0];
+				
+				this.commits.forEach(commit => {
+					var user = 'rkoripalli';
+					if(commit.author != null)
+						user = commit.author.login;
+					if(user === "vivianmnguyen"){
+                                                        commitCounts[0]++;
+                                                }
+                                                if(user === "KaiboCai"){
+                                                        commitCounts[1]++;
+                                                }
+                                                if(user === "NFinks"){
+                                                        commitCounts[2]++;
+                                                }
+                                                if(user === "prilak"){
+                                                        commitCounts[3]++;
+                                                }
+                                                if(user === "NickPattie"){
+                                                        commitCounts[4]++;
+                                                }
+                                                if(user === "rkoripalli"){
+                                                        commitCounts[5]++;
+                                                }	
+						});
+				this.gitAccountList[0].commits = commitCounts[0].toString();
+				this.gitAccountList[1].commits = commitCounts[1].toString();
+				this.gitAccountList[2].commits = commitCounts[2].toString();
+				this.gitAccountList[3].commits = commitCounts[3].toString();
+				this.gitAccountList[4].commits = commitCounts[4].toString();
+				this.gitAccountList[5].commits = commitCounts[5].toString();
+				console.log(commitCounts);
+				this.totalCommits = 0;
+				commitCounts.forEach(count => this.totalCommits += count);
+			}
+			
+		});
+	}
+	public loadGithubRepos() {
+	var page = 0;
+	this.loadService(0);
+	/*
+	this.githubserv.getUserRepos().subscribe((data) => {
+            this.gitAccountList = data;
+		
+            this.gitAccountList.forEach(i => {
                 }
                 this.totalCommits += i.commits;
             });
-        });
+	    });
+	    */
     }
 }
--- sg-angular/src/app/github-stats/github.service.ts
+++ sg-angular/src/app/github-stats/github.service.ts
@@ -21,8 +21,116 @@ export class GithubService {
                         url: p.author.html_url,
                         commits: p.total
                     })));            
-    }
-    getIssues(): Observable<Issue> {
+		    }
+	getUserCommits(urlString: string, page: number){
+		//var urlString = `https://api.github.com/repositories/168768624/commits?page=` + 0;
+		var urlReq = urlString + page; 
+		let commits = [];
+		let commitCounts: number[] = [0, 0, 0, 0, 0, 0];
+		return this.httpClient.get(`${urlReq}`);
+		/*
+        		.subscribe(res => {
+			res.map(item => {commits.push(item)});
+			if(page < 20) {
+			
+				var prev = this.getUserCommits(urlString, ++page);
+				//prev.map(item => {commits.push(item)});
+				//	console.log(prev[0]);
+				for(let commit of prev) {
+					console.log(commit);
+					commits.push(commit);
+				//res.push(commit);
+				}
+			}
+			console.log(commits.length);
+			return res;
+		
+			//return this.get(`${url}&pageSize=2&page=${page}`);
+        	});
+		*/
+		/*
+		try{
+			let i = 0;
+			//while(i < 30) {
+				console.log(urlString);
+				this.httpClient.get(urlString).pipe(
+				map((item: any) => item.map(p => <GitRepoInfo>
+				{
+					user: p.author.login as string,
+					url: p.author.url as string,
+					commits: '1'
+					}))	
+				).subscribe((datas) => {
+					console.log(datas.length);
+					if(datas.length == 0) {
+						
+					}
+					for(let data of datas) {
+						commits.push(data);
+						if(data.user === "vivianmnguyen"){
+							commitCounts[0]++;
+                				}
+                				if(data.user === "KaiboCai"){
+							commitCounts[1]++;
+                				}
+                				if(data.user === "NFinks"){
+							commitCounts[2]++;
+                				}
+                				if(data.user === "prilak"){
+							commitCounts[3]++;
+                				}
+                				if(data.user === "NickPattie"){
+							commitCounts[4]++;
+                				}
+                				if(data.user === "rkoripalli"){
+							commitCounts[5]++;
+                				}
+						let sortedCommits: GitRepoInfo[] = [
+						{
+							user: 'vivianmnguyen',
+							commits: commitCounts[0].toString()	
+						},
+						{
+							user: 'KaiboCai',
+							commits: commitCounts[1].toString()	
+						},
+						{
+							user: 'NFinks',
+							commits: commitCounts[2].toString()	
+						},
+						{
+							user: 'prilak',
+							commits: commitCounts[3].toString()	
+						},
+						{
+							user: 'NickPattie',
+							commits: commitCounts[4].toString()	
+						},
+						{
+							user: 'rkoripalli',
+							commits: commitCounts[5].toString()	
+						}
+						];
+						console.log(sortedCommits);
+						return sortedCommits;
+					}
+				});
+				i++;
+				//}
+		}
+		catch(e){
+		//	console.log(commits);
+		}
+		finally{
+			console.log(commits);
+		//console.log(commits);
+			for(let i of commits) {
+				console.log('made it');
+			}
+			//		console.log(commitCounts);
+			}*/
+	}
+	getIssues(): Observable<Issue> {
 	return this.httpClient.get(`https://api.github.com/repositories/168768624/issues`).
 		pipe(
 			map((item: any) => item.map(p => <Issue>
