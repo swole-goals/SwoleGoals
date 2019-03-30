7d963bca1bb4595c9b1f9e7ac555d0c2c4c11500
Rohan Koripalli <rohan@koripalli.com>
2019-03-30 15:48:33 -0500

    Updated challenge creation and friends services to use environment variables

--- sg-angular/src/app/challenge-creation-menu/challenge-creation.service.ts
+++ sg-angular/src/app/challenge-creation-menu/challenge-creation.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class ChallengeCreationService {
   constructor(private httpClient : HttpClient) { }
   postAPIdata(challengeData){
     console.log(challengeData);
-    return this.httpClient.post('http://localhost:8080/addChallenge', challengeData);
+    return this.httpClient.post(environment.fireStoreURL+'/addChallenge', challengeData);
   }
 //{'exercises' : `${challengeData[0]}`, 'reps' : `${challengeData[1]}`}
 }
