d2776e0006d366a8b9c2df52aa01149b8e9cc7d9
Michael <mdlawrence97@gmail.com>
2019-03-30 21:26:38 -0400

    still working on filters

--- sg-angular/src/app/exercise-list/exercise-list.component.ts
+++ sg-angular/src/app/exercise-list/exercise-list.component.ts
@@ -21,10 +21,10 @@ export class ExerciseListComponent implements OnInit {
 	public muscles: Array<string> = ['Chest', 'Forearms', 'Lats', 'Middle Back', 'Lower Back', 'Neck', 'Quadriceps', 'Hamstrings', 'Calves', 'Triceps', 'Traps', 'Shoulders', 'Abdominals', 'Glutes', 'Biceps', 'Abductors'];
 	public equipments: Array<string> = ['Dumbell', 'Body Only', 'Cable', 'Other', 'Barbell', 'E-Z Curl Bar', 'Machine', 'Medicine Ball', 'Kettlebells', 'Exercise Ball', 'None', 'Foam Ball', 'Bands'];
 		
-	public selectedType;
-	public selectedEquipment;
-	public selectedMuscle;
-	public selectedExercise;
+	public selectedType = '';
+	public selectedEquipment= '';
+	public selectedMuscle = '';
+	public selectedExercise = null;
 	displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
 	constructor(private exerciseService: ExerciseListService) { 
   		this.exerciseService.getExerciseListUnfiltered().subscribe(res => {
