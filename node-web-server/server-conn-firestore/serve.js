89f7839a03cb7f985d03b06c7b3edca628b2cae7
Rohan Koripalli <rkoripalli@hotmail.com>
2019-04-17 23:46:39 -0500

    Fixed missing .json

--- node-web-server/server-conn-firestore/serve.js
+++ node-web-server/server-conn-firestore/serve.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 const fs = require('fs');
 const {Storage} = require('@google-cloud/storage');
 
-const private_key = `./swolegoalsfirestore-4ad9a0ac617c`;
+const private_key = `./swolegoalsfirestore-4ad9a0ac617c.json`;
 if(!fs.existsSync(private_key)){
   const projectId = 'swolegoalsFirestore';
   const storage = new Storage({
