e95d9fa5dda9c75808531f449dcb07b166c8700b
Nick Pattie <32149718+NickPattie@users.noreply.github.com>
2019-04-22 18:45:13 -0500

    Mock data for leaderboards

--- sg-angular/src/app/leaderboard/leaderboard.component.ts
+++ sg-angular/src/app/leaderboard/leaderboard.component.ts
@@ -4,6 +4,7 @@ import {forEach} from "@angular/router/src/utils/collection";
 import {HttpClient} from "@angular/common/http";
 import {GetUsersService} from "../../services/get-users.service";
 import {MatTableDataSource} from "@angular/material";
+import {ResultsService} from "../results/results.service";
 
 export interface groupLeaderboardElement {
   userName: string;
@@ -31,17 +32,56 @@ export class LeaderboardComponent implements OnInit {
   dataSourceGlobal = new MatTableDataSource(GLOBAL_DATA);
   displayedColumnsGroup = ['userName', 'score'];
   displayedColumnsGlobal= ['userName', 'group', 'score'];
+  //scores: number[];
 
   constructor(
     private groupService: GroupService,
-    private getUsersService: GetUsersService
+    private getUsersService: GetUsersService,
+    private resultsService: ResultsService
   ) {  }
 
   ngOnInit() {
     let groupMembers = this.groupService.getGroupMembers();
       GROUP_DATA.splice(0, GROUP_DATA.length);
+
+
+
+      /*let ans = this.resultsService.getListOfUserScores();
+      let length = ans[1];
+      let scores = ans[0];
+
+
+      console.log(length);
+      console.log(scores);
+      console.log(scores.length);
+      console.log(scores.toString());
+      console.log(scores[0]);
+      console.log(scores[1]);*/
+
+
+      let addedScores = [];
+      addedScores.length = groupMembers.length;
+      let index = 0;
+      //console.log(index);
+      /*for (let score of scores){
+        addedScores[(index)%groupMembers.length] += +score;
+        console.log(index);
+        index++
+      }*/
+      console.log('out');
+
+      /*for(let i = 0; i < scores.length; i++){
+        //console.log('in');
+        addedScores[(i%groupMembers.length)] += +scores[i];
+        //console.log(i%groupMembers.length);
+        //console.log(i);
+      }*/
+
+      console.log(addedScores);
+
+
       for (let user of groupMembers) {
-        GROUP_DATA.push({userName: user, score: String(Math.random())});
+        GROUP_DATA.push({userName: user, score: String((Math.random() * 100).toFixed())});
       }
       GROUP_DATA.sort(function (a, b) {
         return <any>b.score - <any>a.score;
@@ -50,11 +90,11 @@ export class LeaderboardComponent implements OnInit {
 
     this.getUsersService.getAllUsers().subscribe((res) => {
       let allUsers = res as Array<any>;
-      console.log(allUsers);
-      console.log(GLOBAL_DATA);
+      //console.log(allUsers);
+      //console.log(GLOBAL_DATA);
       GLOBAL_DATA.splice(0, GLOBAL_DATA.length);
       for (let user of allUsers){
-        GLOBAL_DATA.push({userName: user.email, group: user.groupID, score: String(Math.random())})
+        GLOBAL_DATA.push({userName: user.email, group: user.groupID, score: String((Math.random() * 100).toFixed())})
       }
       GLOBAL_DATA.sort(function (a, b) {
         return <any>b.score - <any>a.score;
