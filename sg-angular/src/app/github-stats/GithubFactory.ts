d2f75675a33a62b7308082daafda1a6baf5a0943
Michael <mdlawrence97@gmail.com>
2019-04-27 18:57:00 -0400

    factory added and compiles

--- sg-angular/src/app/github-stats/GithubFactory.ts
+++ sg-angular/src/app/github-stats/GithubFactory.ts
@@ -1,7 +1,5 @@
-export class GithubFactory {
-  public getAccount(accountName: string): Account {
-    return new Account();
-  }
+export interface GithubFactory {
+  getAccount(accountName: string): Account;
 }
 
 export class SwoleGoalsFactory implements GithubFactory{
@@ -29,7 +27,7 @@ export class SwoleGoalsFactory implements GithubFactory{
   }
 }
 
-export class Account {
+export interface Account {
   user: string;
   url: string;
   commits: string;
@@ -37,110 +35,155 @@ export class Account {
   bio: string;
   image: string;
   responsibilities: string;
-  public static getInstance: Account() {
-    return null;
-  }
 }
 
 export class Vivian implements Account {
+  public user: string;
+  public url: string;
+  public commits: string;
+  public name: string;
+  public bio: string;
+  public image: string;
+  public responsibilities: string;
+  
   private static instance: Account = new Vivian();
   private constructor() {
-    user = 'vivianmnguyen';
-    url = 'https://github.com/vivianmnguyen';
-    commits = '0';
-    name = 'Vivian Nguyen';
-    bio = 'Vivian is an ECE Major graduating in December of 2019';
-    image = '../../assets/headshots/vivian.jpg';
-    responsibilities = "Initialize CloudSQL database. Load web-scraped exercise data into database. " +
+    this.user = 'vivianmnguyen';
+    this.url = 'https://github.com/vivianmnguyen';
+    this.commits = '0';
+    this.name = 'Vivian Nguyen';
+    this.bio = 'Vivian is an ECE Major graduating in December of 2019';
+    this.image = '../../assets/headshots/vivian.jpg';
+    this.responsibilities = "Initialize CloudSQL database. Load web-scraped exercise data into database. " +
     "Write notes for local database development. " +
     "Write a script for automatically loading web-scraped text files. Phase 2: ";
   }
-  public static getInstance: Account() {
+  public static getInstance() : Account {
     return Vivian.instance;
   }
 }
 
 export class Rohan implements Account {
+  public user: string;
+  public url: string;
+  public commits: string;
+  public name: string;
+  public bio: string;
+  public image: string;
+  public responsibilities: string;
+  
   private static instance: Account = new Rohan();
   private constructor() {
-    user = 'rkoripalli';
-    url = 'https://github.com/rkoripalli';
-    commits = '0';
-    name = 'Rohan Koripalli';
-    bio = 'Rohan is an ECE Major graduating in December of 2019';
-    image = '../../assets/headshots/rohan.png';
-    responsibilities = "Create angular project. Setup Bootstrap. Build Splash page and About page " +
+    this.user = 'rkoripalli';
+    this.url = 'https://github.com/rkoripalli';
+    this.commits = '0';
+    this.name = 'Rohan Koripalli';
+    this.bio = 'Rohan is an ECE Major graduating in December of 2019';
+    this.image = '../../assets/headshots/rohan.png';
+    this.responsibilities = "Create angular project. Setup Bootstrap. Build Splash page and About page " +
     "using Github API. Setup routing between Angular components. Phase 2: Moved frontend to AWS. Added Travis CI for testing and automatic deployment to AWS. Setup Angular build and production environments. Worked on the user profile page and new group creation, helped with backend server for Firestore. Phase 3: Created User and Group services for getting data from backend to allow for a centralized location to get this data from different components. Updated UI for consistency across pages. Created Postman tests for backend servers."; 
   }    
-  public static getInstance: Account() {
+  public static getInstance() : Account {
     return Rohan.instance;
   }
 }
 
 export class Michael implements Account {
+  public user: string;
+  public url: string;
+  public commits: string;
+  public name: string;
+  public bio: string;
+  public image: string;
+  public responsibilities: string;
+  
   private static instance: Account = new Michael();
   private constructor() {
-    user = 'prilak';
-    url = 'https://github.com/prilak';
-    commits = '0';
-    name = 'Michael Lawrence';
-    bio = 'Michael is an ECE Major graduating in May of 2019';
-    image = '../../assets/headshots/michael.jpg';
-    responsibilities = "Create map page and exercise list page. " +
+    this.user = 'prilak';
+    this.url = 'https://github.com/prilak';
+    this.commits = '0';
+    this.name = 'Michael Lawrence';
+    this.bio = 'Michael is an ECE Major graduating in May of 2019';
+    this.image = '../../assets/headshots/michael.jpg';
+    this.responsibilities = "Create map page and exercise list page. " +
     "Add issue information on the about page. Work on initializing the scraping script. Phase 2: Added filters and on click functionality to exercise list page. Created the exercise table page. Fixed issue with missing git commits. Fixed the exercise list table duplicates issue. Added tests for the sql database. Phase 3: Finalized format for performance table, Game Map, Exercise Result, Current Exercise. Added exercise selection and results initialization functionality to Game Map. Created Game Map, Exercise Result, and Current Exercise page. Established connection for entering user results on the frontend.";
   }
-  public static getInstance: Account() {
+  public static getInstance() : Account {
     return Michael.instance;
   }
 }
 export class Nick implements Account {
+  public user: string;
+  public url: string;
+  public commits: string;
+  public name: string;
+  public bio: string;
+  public image: string;
+  public responsibilities: string;
+  
   private static instance: Account = new Nick();
   private constructor() {
-    user = 'NickPattie';
-    url = 'https://github.com/NickPattie';
-    commits = '0';
-    name = 'Nick Pattie';
-    bio = 'Nick is a BME Major graduating in May of 2019';
-    image = '../../assets/headshots/nick.png';
-    responsibilities = "Write Python script for webscraping on Bodybuilding.com utilizing selenium. " +
+    this.user = 'NickPattie';
+    this.url = 'https://github.com/NickPattie';
+    this.commits = '0';
+    this.name = 'Nick Pattie';
+    this.bio = 'Nick is a BME Major graduating in May of 2019';
+    this.image = '../../assets/headshots/nick.png';
+    this.responsibilities = "Write Python script for webscraping on Bodybuilding.com utilizing selenium. " +
     "Create test file for web scraping. " +
     "Provide and load in exersise data into MySQL database. Phase 2: Created challenge creation page and saving the challenges in the new firebase datastore. Phase 3: Connected challenge creation to the backend and the rest of the components. Made the leaderboard component. Helped fix/make various http post and get requests to share information between the different components.";
   }
-  public static getInstance: Account() {
+  public static getInstance() : Account {
     return Nick.instance;
   }
 }
 export class Kaibo implements Account {
+  public user: string;
+  public url: string;
+  public commits: string;
+  public name: string;
+  public bio: string;
+  public image: string;
+  public responsibilities: string;
+  
   private static instance: Account = new Kaibo();
   private constructor() {
-    user = 'KaiboCai';
-    url = 'https://github.com/KaiboCai';
-    commits = '0';
-    name = 'Kaibo Cai';
-    bio = 'Kaibo is an ECE Major graduating in December of 2019';
-    image = '../../assets/headshots/kaibo.jpg';
-    responsibilities = "Set up the backend server using nodejs.  " +
+    this.user = 'KaiboCai';
+    this.url = 'https://github.com/KaiboCai';
+    this.commits = '0';
+    this.name = 'Kaibo Cai';
+    this.bio = 'Kaibo is an ECE Major graduating in December of 2019';
+    this.image = '../../assets/headshots/kaibo.jpg';
+    this.responsibilities = "Set up the backend server using nodejs.  " +
     "Connect the backend server with glcoud sql database. " +
     "Create two Get request routers. Create user table in the database. Phase 2: Set up google firestore server, wrote routes for frontend post querying. Helped build login function and user profile page. Phase 3: Added and modified routers for firestore and sql server to ensure front end functionalities. Help with setting up challenge group reference function on front end and firestore server. Help re-setting the dataservice inside the Angular framework.";
   }
-  public static getInstance: Account() {
+  public static getInstance() : Account {
     return Kaibo.instance;
   }
 }
 export class Nicole implements Account {
+  public user: string;
+  public url: string;
+  public commits: string;
+  public name: string;
+  public bio: string;
+  public image: string;
+  public responsibilities: string;
+  
   private static instance: Account = new Nicole();
   private constructor() {
-    user = 'NFinks';
-    url = 'https://github.com/NFinks';
-    commits = '0';
-    name = 'Nicole Finks';
-    bio = 'Nicole is an ECE Major graduating in December of 2019';
-    image = '../../assets/headshots/nicole.jpg';
-    responsibilities = "Create profile page using Bootstrap 4. Create basic aesthetic design to be " +
+    this.user = 'NFinks';
+    this.url = 'https://github.com/NFinks';
+    this.commits = '0';
+    this.name = 'Nicole Finks';
+    this.bio = 'Nicole is an ECE Major graduating in December of 2019';
+    this.image = '../../assets/headshots/nicole.jpg';
+    this.responsibilities = "Create profile page using Bootstrap 4. Create basic aesthetic design to be " +
     "used throughout front end. Standardization of css files. " +
     "Correction of general front-end functionality errors. Phase 2:. Phase 3: ";
   }
-  public static getInstance: Account() {
+  public static getInstance() : Account {
     return Nicole.instance;
   }
 }
--- sg-angular/src/app/github-stats/github-stats.component.ts
+++ sg-angular/src/app/github-stats/github-stats.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from '../github-stats/github.service';
+import { GithubFactory, SwoleGoalsFactory, Account, Vivian, Kaibo, Nick, Nicole, Rohan, Michael } from './GithubFactory';
+
 
 @Component({
 	selector: 'app-github-stats',
@@ -7,119 +9,72 @@ import { GithubService } from '../github-stats/github.service';
 	styleUrls: ['./github-stats.component.css']
 })
 export class GithubStatsComponent implements OnInit {
-	/*
-					if(i.user === "vivianmnguyen"){
-						i.name = "Vivian Nguyen";
-						i.bio = "Vivian is an ECE Major graduating in December of 2019";
-						i.image = "../../assets/headshots/vivian.jpg"
-						i.responsibilities = "Initialize CloudSQL database. Load web-scraped exercise data into database. " +
-						  "Write notes for local database development. " +
-						  "Write a script for automatically loading web-scraped text files"
-					}
-					if(i.user === "KaiboCai"){
-						i.name = "Kaibo Cai";
-						i.bio = "Kaibo is an ECE Major graduating in December of 2019";
-						i.image = "../../assets/headshots/kaibo.jpg"
-						i.responsibilities = "Set up the backend server using nodejs.  " +
-						  "Connect the backend server with glcoud sql database. " +
-						  "Create two Get request routers. Create user table in the database."
-					}
-					if(i.user === "NFinks"){
-						i.name = "Nicole Finks";
-						i.bio = "Nicole is an ECE Major graduating in December of 2019";
-						i.image = "../../assets/headshots/nicole.jpg"
-						i.responsibilities = "Create profile page using Bootstrap 4. Create basic aesthetic design to be " +
-						  "used throughout front end. Standardization of css files. " +
-						  "Correction of general front-end functionality errors."
-					}
-					if(i.user === "prilak"){
-						i.name = "Michael Lawrence";
-						i.bio = "Michael is an ECE Major graduating in May of 2019";
-						i.image = "../../assets/headshots/michael.jpg"
-						i.responsibilities = "Create map page and exercise list page. " +
-						  "Add issue information on the about page. Work on initializing the scraping script."
-					}
-					if(i.user === "NickPattie"){
-						i.name = "Nick Pattie";
-						i.bio = "Nick is a BME Major graduating in May of 2019";
-						i.image = "../../assets/headshots/nick.png"
-						i.responsibilities = "Write Python script for webscraping on Bodybuilding.com utilizing selenium. " +
-						  "Create test file for web scraping. " +
-						  "Provide and load in exersise data into MySQL database."
-					}
-					if(i.user === "rkoripalli"){
-						i.name = "Rohan Koripalli";
-						i.bio = "Rohan is an ECE Major graduating in December of 2019";
-						i.image = "../../assets/headshots/rohan.png"
-						i.responsibilities = "Create angular project. Setup Bootstrap. Build Splash page and About page " +
-						  "using Github API. Setup routing between Angular components."
-	*/
-	public gitAccountList = [
-		{
-			user: 'vivianmnguyen',
-			url: 'https://github.com/vivianmnguyen',
-			commits: '0',
-			name: 'Vivian Nguyen',
-			bio: 'Vivian is an ECE Major graduating in December of 2019',
-			image: '../../assets/headshots/vivian.jpg',
-			responsibilities: "Initialize CloudSQL database. Load web-scraped exercise data into database. " +
-				"Write notes for local database development. " +
-				"Write a script for automatically loading web-scraped text files. Phase 2: "
-		},
-		{
-			user: 'KaiboCai',
-			url: 'https://github.com/KaiboCai',
-			commits: '0',
-			name: 'Kaibo Cai',
-			bio: 'Kaibo is an ECE Major graduating in December of 2019',
-			image: '../../assets/headshots/kaibo.jpg',
-			responsibilities: "Set up the backend server using nodejs.  " +
-				"Connect the backend server with glcoud sql database. " +
-				"Create two Get request routers. Create user table in the database. Phase 2: Set up google firestore server, wrote routes for frontend post querying. Helped build login function and user profile page. Phase 3: Added and modified routers for firestore and sql server to ensure front end functionalities. Help with setting up challenge group reference function on front end and firestore server. Help re-setting the dataservice inside the Angular framework."
-		},
-		{
-			user: 'NFinks',
-			url: 'https://github.com/NFinks',
-			commits: '0',
-			name: 'Nicole Finks',
-			bio: 'Nicole is an ECE Major graduating in December of 2019',
-			image: '../../assets/headshots/nicole.jpg',
-			responsibilities: "Create profile page using Bootstrap 4. Create basic aesthetic design to be " +
-				"used throughout front end. Standardization of css files. " +
-				"Correction of general front-end functionality errors. Phase 2:. Phase 3: "
-		},
-		{
-			user: 'prilak',
-			url: 'https://github.com/prilak',
-			commits: '0',
-			name: 'Michael Lawrence',
-			bio: 'Michael is an ECE Major graduating in May of 2019',
-			image: '../../assets/headshots/michael.jpg',
-			responsibilities: "Create map page and exercise list page. " +
-				"Add issue information on the about page. Work on initializing the scraping script. Phase 2: Added filters and on click functionality to exercise list page. Created the exercise table page. Fixed issue with missing git commits. Fixed the exercise list table duplicates issue. Added tests for the sql database. Phase 3: Finalized format for performance table, Game Map, Exercise Result, Current Exercise. Added exercise selection and results initialization functionality to Game Map. Created Game Map, Exercise Result, and Current Exercise page. Established connection for entering user results on the frontend."
-		},
-		{
-			user: 'NickPattie',
-			url: 'https://github.com/NickPattie',
-			commits: '0',
-			name: 'Nick Pattie',
-			bio: 'Nick is a BME Major graduating in May of 2019',
-			image: '../../assets/headshots/nick.png',
-			responsibilities: "Write Python script for webscraping on Bodybuilding.com utilizing selenium. " +
-				"Create test file for web scraping. " +
-				"Provide and load in exersise data into MySQL database. Phase 2: Created challenge creation page and saving the challenges in the new firebase datastore. Phase 3: Connected challenge creation to the backend and the rest of the components. Made the leaderboard component. Helped fix/make various http post and get requests to share information between the different components."
-		},
-		{
-			user: 'rkoripalli',
-			url: 'https://github.com/rkoripalli',
-			commits: '0',
-			name: 'Rohan Koripalli',
-			bio: 'Rohan is an ECE Major graduating in December of 2019',
-			image: '../../assets/headshots/rohan.png',
-			responsibilities: "Create angular project. Setup Bootstrap. Build Splash page and About page " +
-				"using Github API. Setup routing between Angular components. Phase 2: Moved frontend to AWS. Added Travis CI for testing and automatic deployment to AWS. Setup Angular build and production environments. Worked on the user profile page and new group creation, helped with backend server for Firestore. Phase 3: Created User and Group services for getting data from backend to allow for a centralized location to get this data from different components. Updated UI for consistency across pages. Created Postman tests for backend servers."
-		}
-	];
+  public factory: GithubFactory = new SwoleGoalsFactory();
+  public gitAccountList = [
+  {
+    user: 'vivianmnguyen',
+    url: 'https://github.com/vivianmnguyen',
+    commits: '0',
+    name: 'Vivian Nguyen',
+    bio: 'Vivian is an ECE Major graduating in December of 2019',
+    image: '../../assets/headshots/vivian.jpg',
+    responsibilities: "Initialize CloudSQL database. Load web-scraped exercise data into database. " +
+    "Write notes for local database development. " +
+    "Write a script for automatically loading web-scraped text files. Phase 2: "
+  },
+  {
+    user: 'KaiboCai',
+    url: 'https://github.com/KaiboCai',
+    commits: '0',
+    name: 'Kaibo Cai',
+    bio: 'Kaibo is an ECE Major graduating in December of 2019',
+    image: '../../assets/headshots/kaibo.jpg',
+    responsibilities: "Set up the backend server using nodejs.  " +
+    "Connect the backend server with glcoud sql database. " +
+    "Create two Get request routers. Create user table in the database. Phase 2: Set up google firestore server, wrote routes for frontend post querying. Helped build login function and user profile page. Phase 3: Added and modified routers for firestore and sql server to ensure front end functionalities. Help with setting up challenge group reference function on front end and firestore server. Help re-setting the dataservice inside the Angular framework."
+  },
+  {
+    user: 'NFinks',
+    url: 'https://github.com/NFinks',
+    commits: '0',
+    name: 'Nicole Finks',
+    bio: 'Nicole is an ECE Major graduating in December of 2019',
+    image: '../../assets/headshots/nicole.jpg',
+    responsibilities: "Create profile page using Bootstrap 4. Create basic aesthetic design to be " +
+    "used throughout front end. Standardization of css files. " +
+    "Correction of general front-end functionality errors. Phase 2:. Phase 3: "
+  },
+  {
+    user: 'prilak',
+    url: 'https://github.com/prilak',
+    commits: '0',
+    name: 'Michael Lawrence',
+    bio: 'Michael is an ECE Major graduating in May of 2019',
+    image: '../../assets/headshots/michael.jpg',
+    responsibilities: "Create map page and exercise list page. " +
+    "Add issue information on the about page. Work on initializing the scraping script. Phase 2: Added filters and on click functionality to exercise list page. Created the exercise table page. Fixed issue with missing git commits. Fixed the exercise list table duplicates issue. Added tests for the sql database. Phase 3: Finalized format for performance table, Game Map, Exercise Result, Current Exercise. Added exercise selection and results initialization functionality to Game Map. Created Game Map, Exercise Result, and Current Exercise page. Established connection for entering user results on the frontend."
+  },
+  {
+    user: 'NickPattie',
+    url: 'https://github.com/NickPattie',
+    commits: '0',
+    name: 'Nick Pattie',
+    bio: 'Nick is a BME Major graduating in May of 2019',
+    image: '../../assets/headshots/nick.png',
+    responsibilities: "Write Python script for webscraping on Bodybuilding.com utilizing selenium. " +
+    "Create test file for web scraping. " +
+    "Provide and load in exersise data into MySQL database. Phase 2: Created challenge creation page and saving the challenges in the new firebase datastore. Phase 3: Connected challenge creation to the backend and the rest of the components. Made the leaderboard component. Helped fix/make various http post and get requests to share information between the different components."
+  },
+  {
+    user: 'rkoripalli',
+    url: 'https://github.com/rkoripalli',
+    commits: '0',
+    name: 'Rohan Koripalli',
+    bio: 'Rohan is an ECE Major graduating in December of 2019',
+    image: '../../assets/headshots/rohan.png',
+    responsibilities: "Create angular project. Setup Bootstrap. Build Splash page and About page " +
+    "using Github API. Setup routing between Angular components. Phase 2: Moved frontend to AWS. Added Travis CI for testing and automatic deployment to AWS. Setup Angular build and production environments. Worked on the user profile page and new group creation, helped with backend server for Firestore. Phase 3: Created User and Group services for getting data from backend to allow for a centralized location to get this data from different components. Updated UI for consistency across pages. Created Postman tests for backend servers."
+  }];
 	public totalCommits = 0;
 	public issues;
 	public commits = [];
@@ -138,8 +93,6 @@ export class GithubStatsComponent implements OnInit {
 		var urlString = `https://api.github.com/repositories/168768624/commits?page=`;
 		this.githubserv.getUserCommits(urlString, page).subscribe(item => {
 			var cs = item as Array<any>;
-			//console.log(item.length);
-			//	for(let item of item)
 
 			if (cs.length != null && cs.length > 0) {
 				cs.forEach(commit => { this.commits.push(commit) });
@@ -186,15 +139,5 @@ export class GithubStatsComponent implements OnInit {
 	}
 	public loadGithubRepos() {
 		this.loadService(1);
-		/*
-		this.githubserv.getUserRepos().subscribe((data) => {
-				this.gitAccountList = data;
-			
-				this.gitAccountList.forEach(i => {
-					}
-					this.totalCommits += i.commits;
-				});
-			});
-			*/
 	}
 }
